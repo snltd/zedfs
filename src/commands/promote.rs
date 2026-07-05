09ba86bd84ebad265e8b0e4f42563411b0b2612b
snltd <github@id264.net>
2026-07-05 16:05:19 +0000

    tidy up promote

--- src/commands/promote.rs
+++ src/commands/promote.rs
@@ -6,21 +6,31 @@ use std::fs;
 
 pub fn run(files: Vec<Utf8PathBuf>, opts: &ZpZrOpts) -> anyhow::Result<()> {
     for file in files {
-        promote_file(&file.canonicalize_utf8()?, opts)?;
+        promote_file(
+            &file
+                .canonicalize_utf8()
+                .with_context(|| format!("file does not exist: {file}"))?,
+            opts,
+        )?;
     }
 
     Ok(())
 }
 
 fn promote_file(path: &Utf8Path, opts: &ZpZrOpts) -> anyhow::Result<()> {
-    let target_file = target_file(path)?;
-    let target_dir = target_file.parent().context("cannot get parent")?;
+    let target_file =
+        target_file(path).with_context(|| format!("cannot derive target for {path}"))?;
+
+    let target_dir = target_file
+        .parent()
+        .with_context(|| format!("cannot get parent for of {path}"))?;
 
     if !target_dir.exists() {
         tracing::info!("creating {target_dir}");
 
         if !opts.noop {
-            fs::create_dir_all(target_dir)?;
+            fs::create_dir_all(target_dir)
+                .with_context(|| format!("failed to create dir: {target_dir}"))?;
         }
     }
 
@@ -34,8 +44,9 @@ fn target_file(path: &Utf8Path) -> anyhow::Result<Utf8PathBuf> {
     let zfs_index = components
         .iter()
         .position(|&c| c == Utf8Component::Normal(".zfs"))
-        .context("no .zfs in path")?;
+        .with_context(|| format!("no .zfs directory in path of {path}"))?;
 
+    // remove .zfs/snapshots/<snap_name> from the path
     let target_path = components
         .iter()
         .enumerate()
@@ -59,15 +70,14 @@ mod test {
     fn test_target_file() {
         assert_eq!(
             Utf8PathBuf::from("/test/dir/file"),
-            target_file(&Utf8PathBuf::from("/test/.zfs/snapshot/monday/dir/file")).unwrap()
+            target_file("/test/.zfs/snapshot/monday/dir/file".into()).unwrap()
         );
 
         assert_eq!(
             Utf8PathBuf::from("/test/u01/u02/mtpt/deep/dir/file"),
-            target_file(&Utf8PathBuf::from(
-                "/test/u01/u02/mtpt/.zfs/snapshot/test/deep/dir/file"
-            ))
-            .unwrap()
+            target_file("/test/u01/u02/mtpt/.zfs/snapshot/test/deep/dir/file".into()).unwrap()
         );
+
+        assert!(target_file("/etc/passwd".into()).is_err());
     }
 }
